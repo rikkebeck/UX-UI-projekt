adada4da28bb8d931f9aad816caedda5855d86bb
rikkebeck <eaa24ribk@students.eaaa.dk>
2025-03-04 13:00:05 +0100

    Add files via upload

--- produkter.js
+++ produkter.js
@@ -14,40 +14,42 @@ function changeToPink() {
         let table = document.querySelector(".kolonneTabel");
         table.innerHTML = `
             <tr>
-                <td class="tekstIndholdGreen">Kalorier</td>
+                <td class="tekstIndholdPink">Kalorier</td>
                 <td class="tekstIndhold">200kJ - 50kcal</td>
             </tr>
             <tr>
-                <td class="tekstIndholdGreen">Fedt</td>
+                <td class="tekstIndholdPink">Fedt</td>
                 <td class="tekstIndhold">0.2g</td>
             </tr>
             <tr>
-                <td class="tekstIndholdGreen">Mættede fedtsyrer</td>
+                <td class="tekstIndholdPink">Mættede fedtsyrer</td>
                 <td class="tekstIndhold">0.0g</td>
             </tr>
             <tr>
-                <td class="tekstIndholdGreen">Kulhydrat</td>
+                <td class="tekstIndholdPink">Kulhydrat</td>
                 <td class="tekstIndhold">3.5g</td>
             </tr>
             <tr>
-                <td class="tekstIndholdGreen">Sukkerarter</td>
+                <td class="tekstIndholdPink">Sukkerarter</td>
                 <td class="tekstIndhold">0.5g</td>
             </tr>
             <tr>
-                <td class="tekstIndholdGreen">Protein</td>
+                <td class="tekstIndholdPink">Protein</td>
                 <td class="tekstIndhold">0.6g</td>
             </tr>
             <tr>
-                <td class="tekstIndholdGreen">Salt</td>
+                <td class="tekstIndholdPink">Salt</td>
                 <td class="tekstIndhold">&lt;0,01g</td>
             </tr>
         `;
 
         // Skift beskrivelsen af Zero Pink
         document.querySelector(".tekstBeskrivelseZeroGreen").innerHTML = `
-            Zero Pink er en forfriskende og sprudlende alkoholfri øl med en perfekt balance mellem syrlig grape og let bitterhed fra humlen. Den friske side: En let og perlende øl med friske noter af grape, der giver en sprød og opkvikkende smagsoplevelse.
-            <br><br>Den aromatiske side: En velafstemt kombination af nøje udvalgte humler og let maltprofil, der sikrer dybde og balance i hver tår.
-        `;
+            Zero Pink er en forfriskende og sprudlende alkoholfri øl med en perfekt balance mellem syrlig grape og let bitterhed fra humlen. Med sin frugtige karakter og livlige smagsprofil er den det oplagte valg til
+                både solrige dage, spontane aftener og hyggelige sammenkomster.<br><br>Den friske side: En let og
+                perlende øl med friske noter af grape, der giver en sprød og opkvikkende smagsoplevelse.
+                <br><br>Den aromatiske side: En velafstemt kombination af nøje udvalgte humler og let maltprofil, der
+                sikrer dybde og balance i hver tår.<br><br>  `;
 
         // Skift variantens tilstand til Pink
         currentVariant = "pink";
@@ -108,8 +110,13 @@ function changeToGreen() {
 
         // Skift beskrivelsen af Zero Green
         document.querySelector(".tekstBeskrivelseZeroGreen").innerHTML = `
-            Zero Green er en forfriskende og sprudlende alkoholfri øl med en perfekt balance mellem syrlig lime og let bitterhed fra humlen. Den friske side: En let og perlende øl med friske noter af lime, der giver en sprød og opkvikkende smagsoplevelse.
-            <br><br>Den aromatiske side: En velafstemt kombination af nøje udvalgte humler og let maltprofil, der sikrer dybde og balance i hver tår.
+            Zero Green er en forfriskende og sprudlende alkoholfri øl med en
+                perfekt balance mellem syrlig lime og
+                let bitterhed fra humlen. Med sin frugtige karakter og livlige smagsprofil er den det oplagte valg til
+                både solrige dage, spontane aftener og hyggelige sammenkomster.<br><br>Den friske side: En let og
+                perlende øl med friske noter af lime, der giver en sprød og opkvikkende smagsoplevelse.
+                <br><br>Den aromatiske side: En velafstemt kombination af nøje udvalgte humler og let maltprofil, der
+                sikrer dybde og balance i hver tår.<br><br>
         `;
 
         // Skift variantens tilstand til Green
